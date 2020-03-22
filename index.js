38b4b38ff0838264aaaa71c557b094c09e204e6a
munroe1786 <era3munroe@gmail.com>
2020-03-21 19:13:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const farmAnimals = 'cow horse sheep pig chicken'
+const [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(' ')
+
+const [Bessie, Dolly, Babe, Little] = 'cow sheep pig chicken'.split(' ')
+
+const [black_and_white, black, pink] = 'cow sheep pig'.split(' ')
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+
+const [r, o, y, g, b, , v] = colors
+
+const [, , , , , indg, ] = colors
 
 const muppet = {
   name: 'Kermit',
@@ -9,6 +20,8 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+
+const { name, color, song, job, partner } = muppet
 
 const k_muppet = {
   k_name: 'Kermit',
@@ -24,6 +37,10 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
+
+const { song_2, song_4} = k_muppet.k_album.the_muppet_movie
+
+const { k_color, k_job, k_partner} = k_muppet
 
 // Strings
 
